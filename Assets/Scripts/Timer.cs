10891efd257bb8727291dc44f4b1840f40b0a154
Nandni Solanki <nsolanki@tamu.edu>
2024-10-02 23:21:07 -0500

    added timer script

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI time;
+
+    public float remainingTime = 600; 
+
+    void Start()
+    {
+        time.text = "00:00";
+    }
+
+
+    void Update()
+    {
+        if (remainingTime > 0.9)
+        {
+            remainingTime -= Time.deltaTime;
+
+            float minutes = Mathf.FloorToInt(remainingTime / 60);
+
+            float seconds = Mathf.FloorToInt(remainingTime % 60);
+
+            time.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+
+    }
+}
