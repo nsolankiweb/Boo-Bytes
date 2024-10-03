cf7ffde516bc14429a61d5e73987c5cfe6481db5
gcfidalgo <gc.fidalgo@gmail.com>
2024-10-02 21:52:04 -0500

    Added Environment

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVNsTjRoci8vLy8vQUFBQm1kWWd3RysyczBVV2s3UVlBY2FQa3RFRGZvYk1GNUcxZmU0VjRrc1hzRUhzRjZtdWs3RzdpZFJmKzdYWmJCeTU4a2M3RnI1UHU5UVd2TWM2Qk5qWHVqdGw4dDBsRWVIOE9uUEEvVklMaHpkaXNXb1FYV2U3OTN5WFlQOERaZkUzWU9HOVNNbVpjUFJNYVhyQ0tlaHpHWHhXbHlHZ1VmaGlLTktPWDFNMnB3clJvMWR0R0hIU0dGSjZTZDh3SEtnOHE4UkphWFhjUlpRK25QY1VUQ05jSU4xd08yZFkrSHdZWnVZNjhSK3ZCckZTQ21PempxaHQ2aE1NRGpoNlRLVW1RbytFYmJabjBpYWJYcnZqMmE3QWFpY0Z4M2tESXY3em10Q0ZhVGtCb1Fnd2VHbWFmWko2ZHg3ZUpTOE5aSjZTZWhWOTVnVXA3Ui9lRVB6WGZyOGZ6N0RsM1AzSlBzdmNDaEZ1Y2tScno5clo=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
